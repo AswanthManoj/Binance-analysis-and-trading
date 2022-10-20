f58ce140ed688f9bc14f45c8d45c13390409b18d
ASWANTH C M <aswanthmanoj51@gmail.com>
2022-10-20 23:02:04 +0530

    Add files via upload

---
+++ v5_v4_ single crypto trades/config.py
@@ -0,0 +1,42 @@
+Binance_API_Key = 'iM6fPRCc0eXuqQ80Xf58JxbLcidBhXVqaEkRNJ8sQwmgT8sc2f2J0XgQk4JHWBrE'
+Binance_Secret_Key = 'XEIngC262MxXIOnzDKxi8VW2H9iq07r0f0VsVp0kJOe7ZX56yDl37RXtVJgQHXtr'
+pushbulletToken = 'o.5mdXFyHGTzfea0spUU99C05cHUz9rq6j'
+#pushbulletToken = 'o.VCEzQtdu6J3ez9RIaeXNVHVzh75l4wxu'
+symbols=   ['galausdt',
+            'chrusdt',
+            'maticusdt',
+            'sandusdt',
+            'nearusdt',
+            'atomusdt',
+            'ltcusdt',
+            'solusdt',
+            'adausdt',
+            'flowusdt',
+            'gmxusdt',
+            'linkusdt',
+            'bnbusdt',
+            'ethusdt',
+            'xrpusdt',
+            'apeusdt',
+            'vetusdt',
+            'manausdt',
+            'gmtusdt',
+            'ksmusdt'
+            ]
+symbol = 'ftmusdt'
+timeFrame = '1m'
+timeFrame_main = '3m'
+trade_fee = 0.00075
+#rsi_avg_len = 14
+atr_tune_val = 1
+atr_scaleFactor = 3.5
+atr_period_for_sig = 10
+atrtrailing_length = 5
+atr_avg_length = 12
+atrTakeprofit = atr_scaleFactor*4
+atrStoploss = atr_scaleFactor
+tradeCapital = 1000
+AcBalance=1500
+assetForTracking=0
+tradeData = {'successfull': 0, 'failed': 0}
+isTest = True
