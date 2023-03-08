79acb5cb3fcb2a87ea6cbb86459779eca34f0efd
Aswanth C Manoj <aswanthmanoj51@gmail.com>
2023-03-08 13:53:40 +0530

    Add files via upload

---
+++ Trade bot v5_test_net/config.py
@@ -0,0 +1,95 @@
+Binance_API_Key = '<Binance_API_Key>'
+Binance_Secret_Key = '<Binance_Secret_Key>'
+pushbulletToken = '<pushbulletToken>'
+'''symbols=   ['galausdt',
+            'chrusdt',
+            'maticusdt',
+            'sandusdt',
+            'nearusdt',
+            'atomusdt',
+            'ltcusdt',
+            'solusdt',
+            'adausdt',
+            'flowusdt',
+            'gmxusdt',
+            'linkusdt',
+            'dotusdt',
+            'avaxusdt',
+            'bnbusdt',
+            'ethusdt',
+            'xrpusdt',
+            'apeusdt',
+            'hntusdt',
+            'qntusdt',
+            'vetusdt',
+            'algousdt',
+            'egldusdt',
+            'manausdt',
+            'gmtusdt',
+            'ksmusdt',
+            'manausdt',
+            'ethusdt',
+            'btcusdt',
+            'darusdt',
+            'lrcusdt',
+            'ldousdt',
+            'enjusdt',
+            'klayusdt',
+            'pyrusdt',
+            'aptusdt',
+            'arusdt',
+            'dydxusdt',
+            'trxusdt',
+            'oneusdt',
+            'ilvusdt',
+            'runeusdt',
+            'filusdt',
+            'chzusdt',
+            'neousdt',
+            'uniusdt',
+            'cakeusdt',
+            'injusdt',
+            'snxusdt',
+            'eosusdt',
+            'hbarusdt',
+            'crvusdt',
+            'fttusdt',
+            'wavesusdt',
+            'celrusdt',
+            'xlmusdt',
+            'zilusdt',
+            'thetausdt',
+            'glmrusdt',
+            'kdausdt',
+            'kavausdt',
+            'cotiusdt',
+            'acausdt',
+            'ampusdt',
+            'ognusdt',
+            'aaveusdt',
+            ]'''
+symbols = [ 'galausdt',
+                'chrusdt',
+                'maticusdt',
+                'sandusdt',
+                'nearusdt',
+                'atomusdt',
+                'ltcusdt',
+                'solusdt',
+                'adausdt',
+                'flowusdt'
+            ]
+timeFrame = '1m'
+trade_fee = 0.00075
+atr_tune_val = 1
+atr_scaleFactor = 8
+atr_period_for_sig = 10
+atr_scaleFactor_trailing = 2.5
+atr_avg_length = 12
+atrTakeprofit = atr_scaleFactor*0.75
+atrStoploss = atr_scaleFactor
+tradeCapital = 1000
+AcBalance=1500.8678794191326
+assetForTracking=0
+tradeData = {'successfull': 3, 'failed': 4}
+isTest = True
